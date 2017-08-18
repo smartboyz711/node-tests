75d76cf1f80f4290e8aa04ac1563fd1052834584
smartboyz711 <30695719+smartboyz711@users.noreply.github.com>
2017-08-18 15:46:11 +0700

    describe test

--- server/server.test.js
+++ server/server.test.js
@@ -3,27 +3,32 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return hello world response',function(done){
-    request(app)
-        .get('/')
-        .expect(404)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                error : 'Page not found.'
-            });
-        })
+describe('Server',() => {
+    describe('GET /',() => {
+        it('should return hello world response',function(done){
+            request(app)
+                .get('/')
+                .expect(404)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        error : 'Page not found.'
+                    });
+                })
+            .end(done);
+        });
+    });
+    describe('/users',() => {
+        it('should return users',(done) => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        name : 'theedanai',
+                        age : '24'
+                    });
+                })
         .end(done);
-});
-
-it('should return users',(done) => {
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                name : 'theedanai',
-                age : '24'
-            });
-        })
-        .end(done);
+        });
+    });
 });
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,52 +1,53 @@
 const utils = require('./utils');
 const expect = require('expect');
 
-it('should add two numbers',() => {
-    var res = utils.add(33,11);
-    expect(res).toBe(44).toBeA('number');
-});
+describe('Utils',() => {
 
-it('should async add two numbers',(done) => {
-    utils.asyncAdd(4, 3, (sum) => {
-        expect(sum).toBe(7).toBeA('number');
-        done();
+    describe('#add',() => {
+        it('should async add two numbers',(done) => {
+            utils.asyncAdd(4, 3, (sum) => {
+                expect(sum).toBe(7).toBeA('number');
+                done();
+            });
+        });
+    });
+
+    it('should async square two numbers',(done) => {
+        utils.asyncsquare(4,(square) => {
+            expect(square).toBe(16).toBeA('number');
+            done();
+        });
+    });
+
+    it('should square number',() => {
+        var res = utils.square(11);
+        expect(res).toBe(121).toBeA('number');
+    });
+
+    it('should expect some value',() => {
+        //expect(12).toNotBe(12);
+        //expect({name : 'tonz'}).toEqual({name : 'tonz'});
+        //expect([2,3,4]).toInclude(2);
+        expect({
+            name: 'Andrew',
+            age: 25,
+            location: 'Philadelphia'
+        }).toExclude({
+            age: 24
+        });
+    });
+
+    it('should verify frist and last names are set',() => {
+        var user = {
+            location : 'Bangkok',
+            age : 25
+        };
+        var fullname = 'Theedanai Poomilamnao';
+        var result = utils.setname(user, fullname);
+        expect(result).toInclude({
+            firstName : 'Theedanai',
+            lastName : 'Poomilamnao'
+        });
     });
 });
 
-it('should async square two numbers',(done) => {
-    utils.asyncsquare(4,(square) => {
-        expect(square).toBe(16).toBeA('number');
-        done();
-    });
-});
-
-it('should square number',() => {
-    var res = utils.square(11);
-    expect(res).toBe(121).toBeA('number');
-});
-
-it('should expect some value',() => {
-    //expect(12).toNotBe(12);
-    //expect({name : 'tonz'}).toEqual({name : 'tonz'});
-    //expect([2,3,4]).toInclude(2);
-    expect({
-        name: 'Andrew',
-        age: 25,
-        location: 'Philadelphia'
-    }).toExclude({
-        age: 24
-    });
-});
-
-it('should verify frist and last names are set',() => {
-    var user = {
-        location : 'Bangkok',
-        age : 25
-    };
-    var fullname = 'Theedanai Poomilamnao';
-    var result = utils.setname(user, fullname);
-    expect(result).toInclude({
-        firstName : 'Theedanai',
-        lastName : 'Poomilamnao'
-    });
-});
